833504872547e22f0156d598bc8fc62eb51251eb
rafaelmoraiscunha <rafael.morais@franq.com.br>
2022-01-24 08:11:06 -0300

    Conexao do site com servico de cadastro de convidados

--- src/components/guests/guests.js
+++ src/components/guests/guests.js
@@ -1,7 +1,42 @@
 import React from 'react'
 import './guests.css'
+import InputMask from 'react-input-mask';
+import axios from 'axios'
 
 const Guests = () => {
+    const handleSend = async () => {
+        const name = document.getElementById("nameInput").value
+        const phone = document.getElementById("phoneInput").value
+        document.getElementById("nameInput").value = ""
+        document.getElementById("phoneInput").value = ""
+
+        const confirmed = true
+        const data = JSON.stringify({
+            name,
+            phone,
+            confirmed
+        });
+
+        const config = {
+            method: 'post',
+            url: 'http://ec2-52-67-114-101.sa-east-1.compute.amazonaws.com/guests',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            data: data
+        };
+        console.log(config)
+        await axios(config)
+            .then(function (response) {
+                console.log(JSON.stringify(response.data));
+                window.alert("Mensagem enviada com sucesso")
+
+            })
+            .catch(function (error) {
+                console.log(error);
+                window.alert("É preciso completar todos os campos")
+            });
+    }
     return (
         <div id='guests' className='guests'>
             <div className='guestsText'>
@@ -12,18 +47,16 @@ const Guests = () => {
 
                 <legend>Lista de presença:</legend>
                 <div className="mb-3">
-                    <label for="emailInput" className="form-label">Email</label>
-                    <input type="text" id="emailInput" className="form-control" placeholder="Digite seu email" />
+                    <label for="phonelInput" className="form-label">Telefone</label>
+                    <InputMask mask='(99) 999999999' type="text" id="phoneInput" className="form-control" placeholder="Digite seu telefone" />
                 </div>
                 <div className="mb-3">
-                    <label for="Nome" className="form-label">Convidado</label>
-                    <select id="Nome" className="form-select">
-                        <option>Emillie</option>
-                    </select>
+                    <label for="nameInput" className="form-label">Nome Completo</label>
+                    <input type="text" id="nameInput" className="form-control" placeholder="Digite seu nome completo" />
                 </div>
                 <div className="mb-3">
                 </div>
-                <button type="submit" className="btn btn-dark">Enviar</button>
+                <button type="button" className="btn btn-dark" onClick={handleSend}>Enviar</button>
 
             </form>
         </div>
