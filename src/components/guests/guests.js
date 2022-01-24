582d17b2e0439125584a37af83c45a54f7916de9
rafaelmoraiscunha <rafael.morais@franq.com.br>
2022-01-24 18:21:18 -0300

    Validaçao de dado de telefone

--- src/components/guests/guests.js
+++ src/components/guests/guests.js
@@ -7,40 +7,45 @@ const Guests = () => {
     const handleSend = async () => {
         const name = document.getElementById("nameInput").value
         const phone = document.getElementById("phoneInput").value
-        document.getElementById("nameInput").value = ""
-        document.getElementById("phoneInput").value = ""
+        const regex = /^[(]{0,1}[0-9]{2}[)]{0,1}[-\s\.]{0,1}[0-9]{5}[-\s\.]{0,1}[0-9]{4}$/
 
-        const confirmed = true
-        const data = JSON.stringify({
-            name,
-            phone,
-            confirmed
-        });
-
-        const config = {
-            method: 'post',
-            url: 'http://ec2-52-67-114-101.sa-east-1.compute.amazonaws.com/guests',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            data: data
-        };
-        console.log(config)
-        await axios(config)
-            .then(function (response) {
-                console.log(JSON.stringify(response.data));
-                window.alert("Mensagem enviada com sucesso")
-
-            })
-            .catch(function (error) {
-                console.log(error);
-                window.alert("É preciso completar todos os campos")
+        if (regex.test(phone) && name) {
+            const confirmed = true
+            const data = JSON.stringify({
+                name,
+                phone,
+                confirmed
             });
+
+            const config = {
+                method: 'post',
+                url: 'http://ec2-52-67-114-101.sa-east-1.compute.amazonaws.com/guests',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                data: data
+            };
+            await axios(config)
+                .then(function (response) {
+                    console.log(JSON.stringify(response.data));
+                    window.alert("Mensagem enviada com sucesso")
+                    document.getElementById("nameInput").value = ""
+                    document.getElementById("phoneInput").value = ""
+                })
+                .catch(function (error) {
+                    console.log(error);
+                    window.alert("Erro no cadastro")
+                    document.getElementById("nameInput").value = ""
+                    document.getElementById("phoneInput").value = ""
+                });
+        } else {
+            window.alert("É preciso completar todos os campos")
+        }
     }
     return (
         <div id='guests' className='guests'>
             <div className='guestsText'>
-                <h3>Venha participar desse momento especial conosco. Apenas pedimos que você confirme sua paresença abaixo:</h3>
+                <h3>Venha participar desse momento especial conosco. Apenas pedimos que você confirme sua paresença até o dia 30 de fev de 2022:</h3>
             </div>
 
             <form className='guestsForm'>
@@ -48,7 +53,7 @@ const Guests = () => {
                 <legend>Lista de presença:</legend>
                 <div className="mb-3">
                     <label for="phonelInput" className="form-label">Telefone</label>
-                    <InputMask mask='(99) 999999999' type="text" id="phoneInput" className="form-control" placeholder="Digite seu telefone" />
+                    <InputMask mask='(99) 99999-9999' type="text" id="phoneInput" className="form-control" placeholder="Digite seu telefone" />
                 </div>
                 <div className="mb-3">
                     <label for="nameInput" className="form-label">Nome Completo</label>
